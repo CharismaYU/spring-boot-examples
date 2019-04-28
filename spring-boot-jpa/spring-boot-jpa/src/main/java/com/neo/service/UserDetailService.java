8263264074bfe6c4f2907d5e82c269a7846d7aca
yuxuenan <yuxuenan@foton.com.cn>
2019-04-28 11:07:43 +0800

    去除无效引用

--- spring-boot-jpa/spring-boot-jpa/src/main/java/com/neo/service/UserDetailService.java
+++ spring-boot-jpa/spring-boot-jpa/src/main/java/com/neo/service/UserDetailService.java
@@ -6,5 +6,5 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface UserDetailService {
-    public Page<UserDetail> findByCondition(UserDetailParam detailParam, Pageable pageable);
+    Page<UserDetail> findByCondition(UserDetailParam detailParam, Pageable pageable);
 }
