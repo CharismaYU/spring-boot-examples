350744fb103b922fedc639a9b7454fa5af6f0e59
yuxuenan <yuxuenan@foton.com.cn>
2019-11-04 11:15:54 +0800

    改名

---
+++ spring-boot-kafka/src/main/java/com/neo/com/neo/domain/Message.java
@@ -0,0 +1,24 @@
+package com.neo.com.neo.domain;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author :  yuxuenan 2019年11月04日
+ */
+@Data
+public class Message {
+    /**
+     * id
+     */
+    private Long id;
+    /**
+     * 消息
+     */
+    private String msg;
+    /**
+     * 时间戳
+     */
+    private Date sendTime;
+}
