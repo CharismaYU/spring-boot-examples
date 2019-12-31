d623ea4edaf263352fed9520f61e8257986c01e1
yuxuenan <yuxuenan@foton.com.cn>
2019-12-31 14:48:23 +0800

    fix bug

--- spring-boot-web/src/main/java/com/neo/service/impl/CourseServiceImpl.java
+++ spring-boot-web/src/main/java/com/neo/service/impl/CourseServiceImpl.java
@@ -1,13 +1,16 @@
 package com.neo.service.impl;
 
+import com.neo.Constants;
 import com.neo.model.Course;
 import com.neo.repository.CourseRepository;
 import com.neo.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * @author :  yuxuenan 2019年12月31日
  */
+@Service(Constants.API_BEAN_NAME_PREFIX + "CourseService")
 public class CourseServiceImpl implements CourseService {
     @Autowired
     private CourseRepository repository;
